55ca167e74663e5d19ce85d7c4ec056741785187
2otogizoshi <47193542+2otogizoshi@users.noreply.github.com>
2019-02-06 19:42:43 -0600

    App setup
    setup components folder

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
-//bootcamp react
+//bootcamp react d1
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
 
 import "./styles.css";
-
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
-}
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
